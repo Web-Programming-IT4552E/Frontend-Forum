050cebd013b97c85a1bbae73c8751c6076a83b4f
Evmos <longdieu12x@gmail.com>
2023-07-16 18:37:56 +0700

    darft

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, Typography, Button, Space } from 'antd';
+import React from "react";
+import { Card, Typography, Button, Space } from "antd";
 
 const { Title } = Typography;
 
@@ -7,15 +7,15 @@ const Contact = () => {
   return (
     <Card>
       <Space direction="vertical">
-        <Title style={{ marginBottom: 'initial' }} level={4}>
+        <Title style={{ marginBottom: "initial" }} level={4}>
           We're here
         </Title>
         <Typography>
           Our door is always open for a good cup of coffee anyplace, anytime,
           anybody.
         </Typography>
-        <Button style={{ marginTop: '4px' }} block type="primary">
-          <a href="https://candleinthewindshop.xyz/" target="_blank">
+        <Button style={{ marginTop: "4px" }} block type="primary">
+          <a href="https://uray.store/" target="_blank">
             Contact Us Now!
           </a>
         </Button>
