86e8092948550b332a2db26faeee34845b2c7b2f
Evmos <longdieu12x@gmail.com>
2023-07-17 00:05:02 +0700

    update refresh token

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,19 +3,19 @@ import axios from "axios";
 import { request } from "../utils/request";
 
 export function logOut() {
-	localStorage.removeItem(`${process.env.REACT_APP_PREFIX_LOCAL}_user`);
-	localStorage.removeItem(`${process.env.REACT_APP_PREFIX_LOCAL}_access_token`);
-	localStorage.removeItem(
-		`${process.env.REACT_APP_PREFIX_LOCAL}_refresh_token`
-	);
-	window.location.href = "/login";
+  localStorage.removeItem(`${process.env.REACT_APP_PREFIX_LOCAL}_user`);
+  localStorage.removeItem(`${process.env.REACT_APP_PREFIX_LOCAL}_access_token`);
+  localStorage.removeItem(
+    `${process.env.REACT_APP_PREFIX_LOCAL}_refresh_token`
+  );
+  window.location.href = "/login";
 }
 
 export function storeUserData(data) {
-	localStorage.setItem(
-		`${process.env.REACT_APP_PREFIX_LOCAL}_user`,
-		JSON.stringify(data)
-	);
+  localStorage.setItem(
+    `${process.env.REACT_APP_PREFIX_LOCAL}_user`,
+    JSON.stringify(data)
+  );
 }
 
 // export async function loginService(data) {
@@ -44,37 +44,40 @@ export function storeUserData(data) {
 // }
 
 export async function getToken() {
-	try {
-		const data = await axios.post(`${process.env.REACT_APP_API}/auth/refresh`, {
-			refreshToken: localStorage.getItem(
-				`${process.env.REACT_APP_PREFIX_LOCAL}_refresh_token`
-			),
-			accessToken: localStorage.getItem(
-				`${process.env.REACT_APP_PREFIX_LOCAL}_access_token`
-			),
-		});
-		localStorage.setItem(
-			`${process.env.REACT_APP_PREFIX_LOCAL}_access_token`,
-			data.data.accessToken
-		);
-		localStorage.setItem(
-			`${process.env.REACT_APP_PREFIX_LOCAL}_refresh_token`,
-			data.data.refreshToken
-		);
-		return data.data.accessToken;
-	} catch (err) {
-		// logOut();
-	}
+  try {
+    const data = await axios.post(
+      `${process.env.REACT_APP_API}/auth/refresh-token`,
+      {
+        refreshToken: localStorage.getItem(
+          `${process.env.REACT_APP_PREFIX_LOCAL}_refresh_token`
+        ),
+        accessToken: localStorage.getItem(
+          `${process.env.REACT_APP_PREFIX_LOCAL}_access_token`
+        ),
+      }
+    );
+    localStorage.setItem(
+      `${process.env.REACT_APP_PREFIX_LOCAL}_access_token`,
+      data.data.accessToken
+    );
+    localStorage.setItem(
+      `${process.env.REACT_APP_PREFIX_LOCAL}_refresh_token`,
+      data.data.refreshToken
+    );
+    return data.data.accessToken;
+  } catch (err) {
+    // logOut();
+  }
 }
 export async function loginHandler(data) {
-	return await request(
-		{
-			url: `/auth/login`,
-			method: "POST",
-			data,
-		},
-		false
-	)
-		.then((res) => res)
-		.catch((err) => err?.response);
+  return await request(
+    {
+      url: `/auth/login`,
+      method: "POST",
+      data,
+    },
+    false
+  )
+    .then((res) => res)
+    .catch((err) => err?.response);
 }
